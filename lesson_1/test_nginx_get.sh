fb2def23ad28ab7bea9a90fa0d303babdf326eaa
Dmitriy Budakov <60847626+dbudakov@users.noreply.github.com>
2020-11-02 22:06:10 +0300

    Update test_nginx_get.sh

--- lesson_1/test_nginx_get.sh
+++ lesson_1/test_nginx_get.sh
@@ -7,5 +7,5 @@ PORT=${2:-80}
 HOSTNAME=$3
 exec 3<>/dev/tcp/$IP/$PORT #создание/открытие дескриптора и обращение на конкретный source
 #echo -e "GET / HTTP/1.1\r\nConnection: close\r\n\r\n" >&3
-echo -e "GET / HTTP/1.1\r\nhost: $HOSTNAME\r\nConnection: close\r\n\r\n" >&3 #формирование кастомного заголовка
-cat <&3
+echo -e "GET / HTTP/1.1\r\nhost: $HOSTNAME\r\nConnection: close\r\n\r\n" >&3 #формирование кастомного заголовка и запись данных в дескриптор
+cat <&3 #чтение данных из дескриптора
